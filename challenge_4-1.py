691f5af9367b1322cfe0934db80157fbfc25fd97
奈良 幸子 <snara@ac.d2c.co.jp>
2019-11-29 09:01:02 +0000

    challenge19

---
+++ challenge_4-1.py
@@ -0,0 +1,12 @@
+def squared():
+    """
+    squares x.
+    :param x: int.
+    :return: int x*x
+    
+    """
+    x = int(input("数字を入力してください："))
+    return x ** 2
+
+result = squared()
+print(result)
---
+++ challenge_4-2.py
@@ -0,0 +1,11 @@
+def print_character():
+    """
+    prints characters.
+    :param x: string
+    :return: x
+    """
+    x = str(input('文字列を入力してください：'))
+    print(x)
+
+print_character()
+    
